b7724a5da0223eacb1481f2fe19f445acd187dc1
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-03 12:05:18 +0500

    Add project files.

---
+++ Tyuiu.KushnerovIA.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KushnerovIA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            int x = 2;
+            int y = 3;
+            int z = 4;
+            int res = DataService.Calc(x, y, z);
+            Assert.AreEqual(45, res);
+        }
+    }
+}
---
+++ Tyuiu.KushnerovIA.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KushnerovIA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6;
+            int y = 4;
+            int z = 1;
+            int res = DataService.Calc(x, y, z);
+            Console.WriteLine($"({x} + {y} + {z})* 5 = " + res);
+            Console.ReadKey();
+        }
+    }
+}
